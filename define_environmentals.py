7bb1b85fa842c54ab65e29b70a8f2d53cce1aea6
Leonydis138 <138300852+Leonydis138@users.noreply.github.com>
2024-10-24 11:54:43 +0200

    Create define_environmentals.py

---
+++ define_environmentals.py
@@ -0,0 +1,10 @@
+import os
+
+# Define paths
+os.environ['FLASK_APP'] = 'autonomous_ai.py'
+os.environ['FLASK_ENV'] = 'production'
+os.environ['MODEL_DIR'] = '/content/drive/MyDrive/ai_models'  # Path to your models in Google Drive
+os.environ['DATABASE_URL'] = 'bolt://localhost:7687'  # Adjust if necessary
+os.environ['BLOCKCHAIN_NODE'] = 'https://your.ethereum.node'
+os.environ['NLP_MODEL'] = 'bert-base-uncased'  # Example model
+os.environ['VISION_MODEL'] = 'your_vision_model_name'  # Specify your vision model
