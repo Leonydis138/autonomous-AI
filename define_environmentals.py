4c7cf61b0cfd5685ad71a88c3cbef2f49628b950
Leonydis138 <138300852+Leonydis138@users.noreply.github.com>
2024-10-24 11:58:09 +0200

    Update define_environmentals.py

--- define_environmentals.py
+++ define_environmentals.py
@@ -1,7 +1,7 @@
 import os
 
 # Define paths
-os.environ['FLASK_APP'] = 'autonomous_ai.py'
+os.environ['FLASK_APP'] = 'autonomous_ai_colab.py'
 os.environ['FLASK_ENV'] = 'production'
 os.environ['MODEL_DIR'] = '/content/drive/MyDrive/ai_models'  # Path to your models in Google Drive
 os.environ['DATABASE_URL'] = 'bolt://localhost:7687'  # Adjust if necessary
